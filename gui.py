7c496435463d7fe9edcffed4424dc97717d8c1e9
apenaranda <alejandropenaranda3001@gmail.com>
2023-06-17 15:27:58 -0500

    gui

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import pygame , sys
 from pygame.locals import *
 import math
-from main import (mapa,nodo_raiz,verFuturo)
+from main import (mapa,nodo_raiz,hijoMax,movimientoNegro)
 pygame.font.init()
 
 #se crea el tablero, nota si se actualiza el tablero se debe de referenciar otra vez ya que no esta en el while del refresco
@@ -13,9 +13,7 @@ def create_board (matriz,size):
     for rows in matriz:
         i = i+1
         for cells in rows:
-
             screen.blit(roadImage, ((j*size)+aux,(i*size)+aux))
-            
             if (cells == 1):
                 screen.blit(points1, ((j*size),(i*size)))
             elif (cells == 2):
@@ -61,8 +59,9 @@ def iniciarGUI(nodo):
     pygame.init()
 
     #Configuracion para el texto
-    auxiliar=1
-    pintar_juego(nodo) #pinta el tablero
+    auxiliar=1 
+    pintar_juego(nodo)
+    pintar_score(nodo) #pinta el tablero
 
     #while para la logica o los eventos
     #while auxiliar < len(nodos_lista):
@@ -80,6 +79,15 @@ def iniciarGUI(nodo):
             
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                global pos
+                mousePos = pygame.mouse.get_pos()
+                print('x: ',mousePos[0], 'y:',mousePos[1])
+                pos = checkCasilla(mousePos, [[1,1], [4,4]])
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print('espacio')
+                    pintar_casillas_posibles(nodo,[[1,1],[4,4]])
 
 def pintar_juego(nodo):
     """
@@ -107,8 +115,40 @@ def pintar_juego(nodo):
     screen.blit(blackHorse, ((caballoN[1]*imgsize),(caballoN[0]*imgsize)))
     screen.blit(whiteHorse, ((caballoB[1]*imgsize),(caballoB[0]*imgsize)))
 
-#def pintar_casillas_posibles():
+def pintar_casillas_posibles(nodo,casillas):
+    for casilla in casillas:
+        screen.blit(tileImage, ((casilla[1]*imgsize),(casilla[0]*imgsize)))
+        pygame.display.flip()
+        pygame.display.update()
 
+def checkCasilla(posicion, posibles_movimientos):
+    index = 0
+
+    for i in range(len(posibles_movimientos)):
+       coordx = posibles_movimientos[i][0]
+       coordy = posibles_movimientos[i][1]
+       if posicion[0] > coordx*imgsize and posicion[0] < (coordx+1)*imgsize:
+        if posicion[1] > coordy*imgsize and posicion[1] < (coordy+1)*imgsize:
+            index = i
+            print('casilla correcta pulsada')
+            pintarCaballoN(posibles_movimientos[index])
+            return index
+            
+
+
+def pintar_score(nodo):
+    puntosN = nodo.showPuntuacionN()
+    puntosB = nodo.showPuntuacionB()
+    P1 = font.render('P1: '+ str(puntosB), True, black)
+    P2 = font.render('P2: '+ str(puntosN), True, black)
+    screen.blit(P1,(10,725))
+    screen.blit(P2,(600,725))
+
+
+def pintarCaballoN(pos):
+    screen.blit(blackHorse, ((pos[1]*imgsize),(pos[0]*imgsize)))
+    pygame.display.flip()
+    pygame.display.update()
 #____________________________________________________________________________________________________________
 #------------
 
@@ -120,6 +160,7 @@ green = (0,255,0)
 white = (255,255,255)
     
 fuente = pygame.font.SysFont('Segoe UI',28)
+font = pygame.font.SysFont('arialblack',30)
 fuente2 = pygame.font.SysFont('Segoe UI', 40)
 texto = fuente.render("prueba de texto",True,black)
 
@@ -136,11 +177,12 @@ points6 = pygame.transform.scale(pygame.image.load('imagenes/6.png'), (auxsize,a
 points7 = pygame.transform.scale(pygame.image.load('imagenes/7.png'), (auxsize,auxsize))
 blackHorse = pygame.transform.scale(pygame.image.load('imagenes/CN.png'), (auxsize,auxsize))
 whiteHorse = pygame.transform.scale(pygame.image.load('imagenes/CB.png'), (auxsize,auxsize))
+tileImage = pygame.transform.scale(pygame.image.load('imagenes/casilla.png'), (imgsize,imgsize))
 
 
 #tamanho de la GUI
 aux1 = n*imgsize
-aux2 = m*imgsize
+aux2 = m*imgsize + 50
 size = (aux1,aux2)
 
 
@@ -148,6 +190,7 @@ size = (aux1,aux2)
 screen = pygame.display.set_mode(size)
 
 iniciarGUI(nodo_raiz)
+
 
 
 #se planteo la conexion entre el main y la gui.py, ahora mismo solo se esta printeando el estado inicial, o sea el nodo_raiz cuando se tenga la solucion del problema en un array
